22183c16ba8b0495a3088a1a201687bf568c3c71
Benedikt Schwab <beneschwab92@gmail.com>
2023-03-27 18:20:38 +0200

    added wf name and description

--- pages/Workflows/[id].vue
+++ pages/Workflows/[id].vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { nextTick, onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
-import type { Version } from '@/types/workflow-types'
+import type { Version, Workflow } from '@/types/workflow-types'
 
 const dynamicFormValid = ref(false)
 const route = useRoute()
@@ -10,11 +10,13 @@ const versions = ref<Version[]>([])
 const selectedVersion = ref<Version | undefined>()
 const formData = ref<any>({})
 const tableData = ref<any[]>([])
+const workflows = ref<Workflow[]>([])
 
 // Replace the following with the correct API call to fetch the workflow by UUID
 onMounted(async () => {
   try {
     loadVersions(wf_UUID)
+    loadWorkflows()
   }
   catch (error) {
     console.error('Error loading workflow:', error)
@@ -46,41 +48,64 @@ watch(selectedVersion, () => {
   formData.value = {}
   tableData.value = []
 })
+
+// define const
+
+const searchTerm = ref('')
+
+// functions
+async function loadWorkflows(): Promise<void> {
+  try {
+    const response = await fetch('http://localhost:3000/workflows')
+    workflows.value = await response.json()
+  }
+  catch (error) {
+    console.error('Error loading workflows:', error)
+  }
+}
+
+const currentWorkflow = computed(() => {
+  const currentWorkflow = workflows.value.find(
+    workflow => workflow.UUID === wf_UUID,
+  )
+  return currentWorkflow
+})
 </script>
 
 <template>
+  <v-container class="bg-surface-variant">
+    <div>
+      <h1>{{ currentWorkflow?.name.toUpperCase() }}</h1>
+      <div>{{ currentWorkflow?.description }}</div>
+    </div>
+  </v-container>
   <v-container>
-    <div>
-      <h1>Workflow Data</h1>
+    <WorkflowVersionSelector
+      v-if="versions.length > 1"
+      :versions="versions"
+      @version-selected="setSelectedVersion"
+    />
+    <div v-else-if="versions.length === 1">
+      Version: {{ versions[0].name }}
     </div>
-    <div>
-      <WorkflowVersionSelector
-        v-if="versions.length > 1"
-        :versions="versions"
-        @version-selected="setSelectedVersion"
-      />
-      <div v-else-if="versions.length === 1">
-        Version: {{ versions[0].name }}
+    <div v-if="selectedVersion" class="mt-3">
+      <div class="parameter-section">
+        <h2>Parameter</h2>
+        <WorkflowDynamicForm
+          v-model:model-value="formData"
+          :version="selectedVersion"
+          @update:form-valid="dynamicFormValid = $event"
+        />
       </div>
-      <div v-if="selectedVersion" class="mt-3">
-        <div class="parameter-section">
-          <h2>Parameter</h2>
-          <WorkflowDynamicForm
-            v-model:model-value="formData"
-            :version="selectedVersion"
-            @update:form-valid="dynamicFormValid = $event"
-          />
-        </div>
-        <div class="table-section mt-4">
-          <h2>Table</h2>
-          <WorkflowTableGenerator
-            v-model:table-data="tableData"
-            :table-definition="selectedVersion.design_table_definition"
-          />
-        </div>
+      <div class="table-section mt-4">
+        <h2>Table</h2>
+        <WorkflowTableGenerator
+          v-model:table-data="tableData"
+          :table-definition="selectedVersion.design_table_definition"
+        />
       </div>
-      <pre>{{ JSON.stringify(formData, null, 2) }}</pre>
-      <pre>{{ JSON.stringify(tableData, null, 2) }}</pre>
     </div>
+    <pre>{{ JSON.stringify(formData, null, 2) }}</pre>
+    <pre>{{ JSON.stringify(tableData, null, 2) }}</pre>
   </v-container>
 </template>
