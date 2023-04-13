8323508d8ab2d88e589655487feed4e197107a71
Benedikt Schwab <beneschwab92@gmail.com>
2023-04-13 11:32:04 +0200

    Clear test setup

--- pages/testing/index.vue
+++ pages/testing/index.vue
@@ -1,21 +1,29 @@
-<script setup>
-import { ref } from 'vue'
-import CustomTextInput from './CustomTextInput.vue'
-import Message from './Message.vue'
+<script setup lang="ts">
+import { onMounted, reactive } from 'vue'
+import { SpaceFetchOptions, SpaceSearchCriteria, openbis } from 'openbis'
 
-const msg = ref('msg')
-const firstName = ref('')
+const numOfSpaces = reactive({ value: 0 })
+
+onMounted(async () => {
+  // Instantiate the AS API
+  const v3 = new openbis()
+
+  // Login to obtain a session token
+  await v3.login('admin', 'password')
+
+  // Search for spaces
+  const result = await v3.searchSpaces(new SpaceSearchCriteria(), new SpaceFetchOptions())
+
+  // Set the number of spaces
+  numOfSpaces.value = result.getObjects().length
+
+  // Logout to release the resources related with the session
+  await v3.logout()
+})
 </script>
 
 <template>
-  ]
   <div>
-    <h4>defineModel: </h4>
-    <CustomTextInput v-model:text="firstName" />
-    Name: {{ firstName }}
-  </div>
-  <div>
-    <h4>defineModel: </h4>
-    <Message v-model:msg="msg" />
+    <p>Number of spaces: {{ numOfSpaces }}</p>
   </div>
 </template>
