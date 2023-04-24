c763778fb9c526387daf80941578bd538371a28c
Benedikt Schwab <beneschwab92@gmail.com>
2023-04-24 11:22:04 +0200

    Some testing stuff

---
+++ config.js
@@ -0,0 +1,31 @@
+const require = (function () {
+  // Replace this with the actual base URL of your openBIS API
+  const baseUrl = 'http://localhost:3000/openbis/resources/api/v3'
+
+  return {
+    baseUrl,
+    paths: {
+      'jquery': 'lib/jquery/js/jquery',
+      'stjs': 'lib/stjs/js/stjs',
+      'underscore': 'lib/underscore/js/underscore',
+      'moment': 'lib/moment/js/moment',
+      'components': '../../components',
+      'openbis-screening': '../../js/openbis-screening',
+      'bootstrap': '../../lib/bootstrap/js/bootstrap.min',
+      'bootstrap-slider': '../../lib/bootstrap-slider/js/bootstrap-slider.min',
+    },
+    shim: {
+      'stjs': {
+        exports: 'stjs',
+        deps: ['underscore'],
+      },
+      'underscore': {
+        exports: '_',
+      },
+      'openbis-screening': {
+        deps: ['openbis'],
+        exports: 'openbis',
+      },
+    },
+  }
+})()
--- pages/testing/index.vue
+++ pages/testing/index.vue
@@ -1,27 +1,24 @@
-<script>
-// With "require" call we asynchronously load "openbis", "SpaceSearchCriteria" and "SpaceFetchOptions" classes that we will need for our example.
-// The function that is passed as a second parameter of the require call is a callback that gets executed once requested classes are loaded.
-// In Javascript we work with exactly the same classes as in Java. For instance, "ch.ethz.sis.openbis.generic.asapi.v3.dto.space.search.SpaceSearchCriteria"
-// Java class and "as/dto/space/search/SpaceSearchCriteria" Javascript class have exactly the same methods. In order to find a Javascript class name please
-// check our Javadoc (https://openbis.ch/javadoc/20.10.x/javadoc-api-v3/index.html). The Javascript class name is defined in @JsonObject annotation of each V3 API Java DTO.
+<script setup>
+import useOpenBisApi from '../composables/useOpenBisApi'
 
-require(['openbis', 'as/dto/space/search/SpaceSearchCriteria', 'as/dto/space/fetchoptions/SpaceFetchOptions'], (openbis, SpaceSearchCriteria, SpaceFetchOptions) => {
-  // get a reference to AS API
-  const v3 = new openbis()
+const { connect, disconnect, searchSpaces } = useOpenBisApi()
 
-  // login to obtain a session token (the token it is automatically stored in openbis object and will be used for all subsequent API calls)
-  v3.login('admin', 'password').done(() => {
-    // invoke other API methods, for instance search for spaces
-    v3.searchSpaces(new SpaceSearchCriteria(), new SpaceFetchOptions()).done((result) => {
-      alert(`Number of spaces: ${result.getObjects().length}`)
-
-      // logout to release the resources related with the session
-      v3.logout()
-    })
-  })
-})
+const fetchSpaces = async () => {
+  try {
+    await connect()
+    const spaces = await searchSpaces()
+    console.log('Spaces:', spaces)
+    await disconnect()
+  }
+  catch (error) {
+    console.error('Error:', error)
+  }
+}
 </script>
 
 <template>
-  <div />
+  <button @click="fetchSpaces">
+    Search Spaces
+  </button>
 </template>
+In this example, I changed the name of the method in the component from searchSpaces to fetchSpaces to avoid the naming conflict.
