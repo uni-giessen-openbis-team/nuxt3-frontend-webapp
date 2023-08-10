436f5678737884573e30f4dfc551f5aae5044625
Benedikt Schwab <beneschwab92@gmail.com>
2023-08-10 13:25:18 +0200

    Tinker around with test

--- components/Wizzard/__tests__/ProjectContext.spec.ts
+++ components/Wizzard/__tests__/ProjectContext.spec.ts
@@ -1,18 +1,37 @@
-import { test } from 'vitest'
-import { mockNuxtImport, registerEndpoint } from 'nuxt-vitest/utils'
+import { describe, expect, it } from 'vitest'
+import { mountSuspended } from 'vitest-environment-nuxt/utils'
+import ProjectContext from '@/components/Wizzard/ProjectContext.vue'
 
-test('my test', () => {
-  // ... test with Nuxt environment!
-})
+describe('ProjectContext.vue', () => {
+  it('renders correctly with initial state', async () => {
+    const component = await mountSuspended(ProjectContext, {
+      props: {
+        modelValue: { space: '' },
+      },
+    })
 
-// mock storage
-mockNuxtImport('useStorage', () => {
-  return () => {
-    return { value: 'mocked storage' }
-  }
-})
+    // Check if the correct labels exist
+    expect(component.html()).toContain('New Project Name')
+    expect(component.html()).toContain('Contact Person')
+    expect(component.html()).toContain('Project Manager')
+    expect(component.html()).toContain('Description')
 
-// mock API data
-registerEndpoint('/test/', () => {
-  test: 'test-field'
+    // ... additional assertions
+  })
+
+  it('validates New Project Name correctly', async () => {
+    const component = await mountSuspended(ProjectContext)
+    const textField = component.find('v-text-field[label="New Project Name"]')
+
+    // Set value to an empty string and trigger validation
+    textField.setValue('Test Project Name')
+    await component.vm.$nextTick()
+
+    // This assumes that invalid input will modify the HTML in some way to indicate an error
+    expect(textField.html()).toContain('Test Project Name')
+
+    // ... additional assertions
+  })
+
+  // ... other tests similar to your example
 })
