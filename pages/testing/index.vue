7bc49522f7eedebf0b7f3f445592446e226dd006
Benedikt Schwab <beneschwab92@gmail.com>
2023-09-19 10:00:48 +0200

    test APIComponentsAutocompleteSpaces

--- pages/testing/index.vue
+++ pages/testing/index.vue
@@ -1,76 +1,14 @@
-<script>
-import draggable from 'vuedraggable'
-export default {
-  name: 'App',
-  components: { Draggable: draggable },
-  data() {
-    return {
-      people: [
-        { name: 'Abby' },
-        { name: 'Brooke' },
-        { name: 'Courtenay' },
-        { name: 'David' },
-      ],
-      places: [
-        { name: 'Austria' },
-        { name: 'Bahamas' },
-        { name: 'Canada' },
-        { name: 'Djibouti' },
-      ],
-    }
-  },
-}
+<script setup lang="ts">
+
 </script>
 
 <template>
-  <div id="app">
-    <table>
-      <thead>
-        <tr>
-          <th>Table where sorting works.  No transition group</th>
-        </tr>
-      </thead>
-      <Draggable :list="people" element="tbody">
-        <tr v-for="person in people">
-          <td>{{ person.name }}</td>
-        </tr>
-      </Draggable>
-      <tbody>
-        <tr><td>Non-moving tbody goes here</td></tr>
-      </tbody>
-    </table>
-    <table>
-      <thead>
-        <tr>
-          <th>Table with sorting and transition group</th>
-        </tr>
-      </thead>
-      <Draggable :list="places" element="tbody">
-        <transition-group tag="div" name="list-complete">
-          <tr v-for="country in places" :key="country.name" class="list-complete-item">
-            <td>{{ country.name }}</td>
-          </tr>
-        </transition-group>
-      </Draggable>
-      <tbody>
-        <tr><td>Non-moving tbody goes here</td></tr>
-      </tbody>
-    </table>
+  <div>
+    Testing
   </div>
+  <APIComponentsAutocompleteSpaces />
 </template>
 
-<style>
-table{ border: 1px solid black; margin: 2em; }
+<style lang="scss" scoped>
 
-.list-complete-item {
-  transition: all 1s;
-}
-
-.list-complete-enter, .list-complete-leave-active {
-  opacity: 0;
-  height: 0px;
-  margin-top: 0px;
-  padding: 0px;
-  border: solid 0px;
-}
 </style>
