782cc61be4d9764711f5649d6e898638b7cdaee5
Benedikt Schwab <beneschwab92@gmail.com>
2024-05-21 08:18:45 +0000

    Updated ProjectEnteties.vue to use a generic array type for modelValue and variables.

--- components/Wizzard/ProjectEnteties.vue
+++ components/Wizzard/ProjectEnteties.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-type ProjectEntity = {
-  title: string;
-  conditions: Array<any>;
-  continous: boolean;
-  unit: string | null;
-}
-
-const store = useOpenBisStore()
-const  modelValue  = defineModel<{ modelValue: ProjectEntity[] }>()
+const  modelValue  = defineModel<{ modelValue: Array<Object> }>()
 
 const tab = ref('')
 
-const variables = ref<ProjectEntity[]>([
+const variables = ref([
   {
     title: 'genotype',
     conditions: ref([]),
