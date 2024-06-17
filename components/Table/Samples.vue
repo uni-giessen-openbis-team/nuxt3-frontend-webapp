087e8818c4c661b3a2953e9a683da81e837547e0
Benedikt Schwab <beneschwab92@gmail.com>
2024-06-17 22:14:00 +0000

    aider: Create a table in the `components/Table/Samples.vue` file to display samples passed as a prop.
    # Aider chat conversation:
    
    USER: create a table in the `components/Table/Samples.vue` file which will get samples as a prop and show them. the samples objects look like thisL
    
    ASSISTANT: To create a table in the `components/Table/Samples.vue` file that receives samples as a prop and displays them, we need to:
    
    1. Define the props in the `<script>` section.
    2. Create a table structure in the `<template>` section to display the samples.
    
    Here is the *SEARCH/REPLACE* block for the `components/Table/Samples.vue` file:
    
    components/Table/Samples.vue
    ```vue
    <<<<<<< SEARCH
    =======
    <template>
      <v-container>
        <v-simple-table>
          <thead>
            <tr>
              <th>Sample Code</th>
              <th>Description</th>
            </tr>
          </thead>
          <tbody>
            <tr v-for="sample in samples" :key="sample.getPermId().toString()">
              <td>{{ sample.getCode() }}</td>
              <td>{{ sample.getProperties().description }}</td>
            </tr>
          </tbody>
        </v-simple-table>
      </v-container>
    </template>
    
    <script setup lang="ts">
    import { defineProps } from 'vue'
    import openbis from '@/composables/openbis.esm'
    
    const props = defineProps<{
      samples: openbis.Sample[]
    }>()
    </script>
    
    <style scoped>
    </style>
    >>>>>>> REPLACE
    ```
    
    This will create a table that displays the sample code and description for each sample passed as a prop.

---
+++ components/Table/Samples.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-simple-table>
+      <thead>
+        <tr>
+          <th>Sample Code</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="sample in samples" :key="sample.getPermId().toString()">
+          <td>{{ sample.getCode() }}</td>
+          <td>{{ sample.getProperties().description }}</td>
+        </tr>
+      </tbody>
+    </v-simple-table>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import openbis from '@/composables/openbis.esm'
+
+const props = defineProps<{
+  samples: openbis.Sample[]
+}>()
+</script>
+
+<style scoped>
+</style>
